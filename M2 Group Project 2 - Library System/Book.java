588ca4f128365775db3915baa92f11f3c9feff9a
Isabel C. Morales <isabel.morales@robinsonsbank.com.ph>
2025-12-18 17:51:21 +0800

    Added new folders to branch isabelbelbel

--- M2 Group Project 2 - Library System/Book.java
+++ M2 Group Project 2 - Library System/Book.java
@@ -5,7 +5,9 @@ public class Book {
 	
 	// adding this "id" to give you an idea on what options you can do
 	
+	private String author;
 	private Boolean isAvailable = true;
+	private Integer id;
 	
 	public Boolean getIsAvailable() {
 		return isAvailable;
@@ -13,43 +15,32 @@ public class Book {
 	public void setIsAvailable(Boolean isAvailable) {
 		this.isAvailable = isAvailable;
 	}
-
-
-	private Integer id;
+	
 	public Integer getId() {
 		return id;
 	}
 
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 
 	private String title;
 	public String getTitle() {
 		return title;
 	}
 
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 
 	public String getAuthor() {
 		return author;
 	}
 
-
 	public void setAuthor(String author) {
 		this.author = author;
 	}
 
-
-	private String author;
-	
-	
 	public Book (Integer id, String title, String author) {
 		
 		this.id = id;
