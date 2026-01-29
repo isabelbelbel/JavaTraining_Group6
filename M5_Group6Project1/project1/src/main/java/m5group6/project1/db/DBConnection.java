8979cb7743b39e9ed44334a6a66f05f59715b8f6
John Isiah  A. Sanchez <jiasanchez@headoffice.corp.bpi.com.ph>
2026-01-29 11:05:42 +0800

    Update connection to library_db

--- M5_Group6Project1/project1/src/main/java/m5group6/project1/db/DBConnection.java
+++ M5_Group6Project1/project1/src/main/java/m5group6/project1/db/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
 	//Database connection
     public static Connection getConnection() {
         try {
-            String url = "jdbc:postgresql://localhost:5432/training_db"; //
+            String url = "jdbc:postgresql://localhost:5432/library_db"; //
             String user = "jiasanchez"; //Change to local User 
             String pass = "postgres"; //Change to local Password
             return DriverManager.getConnection(url, user, pass);
